08b23ed6e22a3700b43585a51042808528499eb1
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-10-20 19:22:10 -0400

    Time: 163 ms (37.96%), Space: 45.8 MB (5.10%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -7,14 +7,9 @@ var twoSum = function(nums, target) {
     let temp= new Map(nums.map((x,i)=>{
                 return [x,i];
     }))
-    // for (let index = 0; index < nums.length; index++) {/* Time O(N) */
-    //     if (temp.has(target - nums[index]) && (temp.get(target - nums[index]) !== index))
-    //         return [ index, temp.get(target - nums[index]) ]
-    // }
     for(let i=0;i<nums.length;i++){
         if(temp.has(target-nums[i]) && temp.get(target-nums[i])!=i){
             return [i,temp.get(target-nums[i])];
         }
     }
-
 };
