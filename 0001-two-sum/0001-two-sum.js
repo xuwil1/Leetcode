84b839f69219df6dc7ce762a75e1dcfeee07c36d
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-10-20 19:15:53 -0400

    Time: 68 ms (97.07%), Space: 44.8 MB (10.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -7,18 +7,14 @@ var twoSum = function(nums, target) {
     let temp= new Map(nums.map((x,i)=>{
                 return [x,i];
     }))
-    for (let index = 0; index < nums.length; index++) {/* Time O(N) */
-        const complement = target - nums[index];
-        const sumIndex = temp.get(complement);
-
-        if (temp.has(target - nums[index]) && (temp.get(target - nums[index]) !== index))
-            return [ index, temp.get(target - nums[index]) ]
-    }
-    // for(let i=0;i<nums.length;i++){
-    //     if(temp.has(target-nums[i]) && temp.get(target-nums[i])!=i){
-    //         return [i,temp.get(target-nums[i])];
-    //     }
+    // for (let index = 0; index < nums.length; index++) {/* Time O(N) */
+    //     if (temp.has(target - nums[index]) && (temp.get(target - nums[index]) !== index))
+    //         return [ index, temp.get(target - nums[index]) ]
     // }
-    //         const isTarget = map.has(complement) && (map.get(complement) !== index)
-    //     if (isTarget) return [ index, sumIndex ]
+    for(let i=0;i<nums.length;i++){
+        if(temp.has(target-nums[i]) && temp.get(target-nums[i])!=i){
+            return [i,temp.get(target-nums[i])];
+        }
+    }
+
 };
