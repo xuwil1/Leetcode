1de1289d15389c33efccc278cb59c7acc6f40a80
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-10-20 18:52:24 -0400

    Time: 114 ms (64.45%), Space: 46.2 MB (5.10%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,21 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i=0;i<nums.length-1;i++){
-        let j=nums.length;
-        console.log(i);
-        while(i<j){
-            if(nums[i]+nums[j]==target){
-                return [i,j];
-            }
-            j--;
-        }
+    let temp= new Map(nums.map((x,i)=>{
+                return [x,i];
+    }))
+    for (let index = 0; index < nums.length; index++) {/* Time O(N) */
+        const complement = target - nums[index];
+        const sumIndex = temp.get(complement);
+
+        const isTarget = temp.has(complement) && (temp.get(complement) !== index)
+        if (isTarget) return [ index, sumIndex ]
     }
+    // for(let i=0;i<nums.length;i++){
+    //     if(temp.has(target-nums[i]) && temp.get(target-nums[i])!=i){
+    //         return [temp.get(nums[i]),temp.get(target-nums[i])];
+    //     }
+    // }
+    //         const isTarget = map.has(complement) && (map.get(complement) !== index)
+    //     if (isTarget) return [ index, sumIndex ]
 };
