dfab43c614a2d5c88c79daebd72bb726df517cc9
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-10-27 16:12:30 -0400

    Time: 137 ms (30.30%), Space: 51.1 MB (10.72%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -13,7 +13,8 @@ const isValid = (s) => {
     let [ left, right ] =  [ 0, (s.length - 1) ];
 
     while (left < right) {                          /* Time O(N) */
-        [ left, right ] = moveToMid(s, left, right);/* Time O(N) */
+           while ((left < right) && !isAlphaNumeric(s[left])) left++;  /* Time O(N) */
+    while ((left < right) && !isAlphaNumeric(s[right])) right--;/* Time O(N) */
 
         const [ leftCode, rightCode ] = getCodes(s, left, right);
   
@@ -26,12 +27,7 @@ const isValid = (s) => {
     return true;
 }
 
-const moveToMid = (s, left, right) => {
-    while ((left < right) && !isAlphaNumeric(s[left])) left++;  /* Time O(N) */
-    while ((left < right) && !isAlphaNumeric(s[right])) right--;/* Time O(N) */
-    
-    return [ left, right ];
-}
+
 
 const getCodes = (s, left, right) => [ getCode(s[left]), getCode(s[right]) ];
 
