c779c66b7b64911b4ccd48b2ba70222e5b55155e
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-10-27 16:21:47 -0400

    Time: 1017 ms (5.00%), Space: 49.7 MB (17.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -2,40 +2,28 @@
  * @param {string} s
  * @return {boolean}
  */
-
 var isPalindrome = function(s) {
-    if (!s.length) return true;
+    let left=0;
+    let right=s.length-1;
     s=s.toLowerCase();
-    return isValid(s);/* Time O(N) */
+    while(left<right){
+        while(left<right && !check(s[left])){
+            left++;                 
+        }
+        while(left<right && !check(s[right])){
+            right--;                 
+        }
+        if(s[left]!==s[right]) return false;
+        left++;
+        right--;
+
+    }
+    return true;
 };
 
-const isValid = (s) => {
-    let [ left, right ] =  [ 0, (s.length - 1) ];
 
-    while (left < right) {                          /* Time O(N) */
-           while ((left < right) && !isAlphaNumeric(s[left])) left++;  /* Time O(N) */
-           while ((left < right) && !isAlphaNumeric(s[right])) right--;/* Time O(N) */
-
-//         const [ leftCode, rightCode ] = getCodes(s, left, right);
-  
-
-        if (s[left] !== s[right]) return false;
-
-        left++; right--;
-    }
-
-    return true;
-}
-
-
-
-const getCodes = (s, left, right) => [ getCode(s[left]), getCode(s[right]) ];
-
-const getCode = (char) => char.toLowerCase().charCodeAt(0);
-
-const isAlphaNumeric = (c) => {
+function check(c){
     let alpha = c.charCodeAt(0)>='a'.charCodeAt(0) && c.charCodeAt(0)<='z'.charCodeAt(0);
     let num = c.charCodeAt(0)>='0'.charCodeAt(0) &&c.charCodeAt(0)<='9'.charCodeAt(0);
-
-    return alpha || num;
-};
+    return alpha || num
+}
