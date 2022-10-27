87db2d05f33c38403e718cfb5a50790c5175f401
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-10-27 16:11:27 -0400

    Time: 169 ms (9.45%), Space: 50.8 MB (11.96%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -2,39 +2,49 @@
  * @param {string} s
  * @return {boolean}
  */
+
 var isPalindrome = function(s) {
-    let left=0;
-    let right=s.length-1;
-    s=s.toLowerCase();
-    console.log(s);
-    while(left<right){
-// if(!(s[left].charCodeAt(0)>='a'.charCodeAt(0) && s[left].charCodeAt(0)<='z'.charCodeAt(0)) || 
-//     !(s[left].charCodeAt(0)>='0'.charCodeAt(0) && s[left].charCodeAt(0)<='9'.charCodeAt(0))){
-    while(left<right && !check(s[left])){
-            left++;                 
-        }
-// if((s[right].charCodeAt(0)>='a'.charCodeAt(0) && s[right].charCodeAt(0)<='z'.charCodeAt(0)) || 
-//     (s[right].charCodeAt(0)>='0'.charCodeAt(0) && s[right].charCodeAt(0)<='9'.charCodeAt(0))){
-        while(left<right && !check(s[right])){
-            right--;                 
-        }
-        console.log(s[left]+' '+s[right]);
-        if(s[left]==s[right]){
-            left++;
-            right--;
-        }else{
-            return false;
-        }
-    }
-    if(left==right){
-        return true;
-    }
-    return true;
+    if (!s.length) return true;
+
+    return isValid(s);/* Time O(N) */
 };
 
+const isValid = (s) => {
+    let [ left, right ] =  [ 0, (s.length - 1) ];
 
-function check(c){
-    let alpha = c.charCodeAt(0)>='a'.charCodeAt(0) && c.charCodeAt(0)<='z'.charCodeAt(0);
-    let num = c.charCodeAt(0)>='0'.charCodeAt(0) &&c.charCodeAt(0)<='9'.charCodeAt(0);
-    return alpha || num
+    while (left < right) {                          /* Time O(N) */
+        [ left, right ] = moveToMid(s, left, right);/* Time O(N) */
+
+        const [ leftCode, rightCode ] = getCodes(s, left, right);
+  
+        const isEqual = leftCode === rightCode;
+        if (!isEqual) return false;
+
+        left++; right--;
+    }
+
+    return true;
 }
+
+const moveToMid = (s, left, right) => {
+    while ((left < right) && !isAlphaNumeric(s[left])) left++;  /* Time O(N) */
+    while ((left < right) && !isAlphaNumeric(s[right])) right--;/* Time O(N) */
+    
+    return [ left, right ];
+}
+
+const getCodes = (s, left, right) => [ getCode(s[left]), getCode(s[right]) ];
+
+const getCode = (char) => char.toLowerCase().charCodeAt(0);
+
+const isAlphaNumeric = (char) => {
+    const code = getCode(char);
+    
+    const [ a, z ] = [ 97, 122 ];
+    const isAlpha = (a <= code) && (code <= z);
+
+    const [ zero, nine ] = [ 48, 57 ];
+    const isNumeric = (zero <= code) && (code <= nine);
+
+    return isAlpha || isNumeric;
+};
