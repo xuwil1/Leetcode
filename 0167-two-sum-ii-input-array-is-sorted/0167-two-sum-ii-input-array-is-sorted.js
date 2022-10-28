67a927a6c7b4f73b9fa8588a8a9f37d3b1610bbb
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-10-28 12:54:35 -0400

    Time: 306 ms (5.11%), Space: 51.1 MB (6.34%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let [left,right]= [0,numbers.length-1];
+    while(numbers[left]+numbers[right]!== target){
+        if(numbers[left]+numbers[right] < target){ left++;console.log(left);
+                                                 }
+        if(numbers[left]+numbers[right] > target) right--;
+    }
+    return [left+1,right+1]
+};
