a22f9b2238ad927d2111af32a51ca70537827e1f
Willie Xu <73079502+xuwil1@users.noreply.github.com>
2022-11-03 10:19:03 -0400

    Time: 149 ms (62.59%), Space: 47.4 MB (60.26%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -4,13 +4,14 @@
  */
 var lengthOfLongestSubstring = function(s) {
     let set=new Set();
-    let [max,l]=[0,0];
-    for(let r=0; r < s.length; r++){
-        while(set.has(s[r])){
-            set.delete(s[l]);
-            l++;
+    let left=0;
+    let max=0;
+    for(let right=0;right<s.length;right++){
+        while(set.has(s[right])){
+            set.delete(s[left]);
+            left++;
         }
-        set.add(s[r]);
+        set.add(s[right]);
         max=Math.max(max,set.size);
     }
     return max;
